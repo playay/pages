55663d0baae7d44cad6dc6974e6b92f31a6a1d2e
chenyan <chenyan@feling.net>
2015-05-15 09:04:46 +0800

    ....

--- js/feling.js
+++ js/feling.js
@@ -1,11 +1,11 @@
 $(function() {
     $(window).scroll(function() {
-        if ($(window).scrollTop() > 900)
+        if ($(window).scrollTop() > 800)
             $('div.go-top').show();
         else
             $('div.go-top').hide();
     });
     $('div.go-top').click(function() {
-        $('html, body').animate({scrollTop: 0}, 600);
+        $('html, body').animate({scrollTop: 0}, 500);
     });
 });
