dd4e288e5d85ff130bd9c24763bd23e4bbd8fbc2
chenyan <chenyan@feling.net>
2015-05-17 22:51:07 +0800

    ...

--- js/toc.js
+++ js/toc.js
@@ -62,7 +62,7 @@
       }
       if (this_level === level){ // same level as before; same indenting
         html += "<li><a href='#" + fixedEncodeURIComponent(header.id) + "'>" + header.innerHTML + "</a>";
-        console.log(header.innerHTML.replace(\>(.+)\<,"$1"));}
+        console.log(header.innerHTML.replace(new RegExp(">(.+?)<"),"$1"));}
       else if (this_level <= level){ // higher level than before; end parent ol
         for(i = this_level; i < level; i++) {
           html += "</li></"+settings.listType+">"
